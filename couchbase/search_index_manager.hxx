0ec1d7d2a90e5db7d21555810f7aa3b618efbe96
Mateusz <matt.wozakowski@gmail.com>
2023-11-23 16:15:21 +0000

    Add docstrings

--- couchbase/search_index_manager.hxx
+++ couchbase/search_index_manager.hxx
@@ -50,16 +50,45 @@ class cluster;
 class search_index_manager
 {
   public:
+    /**
+     * Fetches an index from the server if it exists
+     *
+     * @param index_name the name of the index
+     * @param options optional parameters
+     * @param handler  handler that implements @ref get_search_index_handler
+     *
+     * @since 1.0.0
+     * @committed
+     */
     void get_index(std::string index_name, const get_search_index_options& options, get_search_index_handler&& handler) const;
 
     [[nodiscard]] auto get_index(std::string index_name, const get_search_index_options& options = {}) const
       -> std::future<std::pair<manager_error_context, management::search::index>>;
 
+    /**
+     * Fetches all indexes from the server
+     *
+     * @param options optional parameters
+     * @param handler  handler that implements @ref get_all_search_indexes_handler
+     *
+     * @since 1.0.0
+     * @committed
+     */
     void get_all_indexes(const get_all_search_indexes_options& options, get_all_search_indexes_handler&& handler) const;
 
     [[nodiscard]] auto get_all_indexes(const get_all_search_indexes_options& options = {}) const
       -> std::future<std::pair<manager_error_context, std::vector<management::search::index>>>;
 
+    /**
+     * Creates, or updates an index
+     *
+     * @param search_index the index definition including name and settings
+     * @param options optional parameters
+     * @param handler  handler that implements @ref upsert_search_index_handler
+     *
+     * @since 1.0.0
+     * @committed
+     */
     void upsert_index(const management::search::index& search_index,
                       const upsert_search_index_options& options,
                       upsert_search_index_handler&& handler) const;
@@ -67,11 +96,31 @@ class search_index_manager
     [[nodiscard]] auto upsert_index(const management::search::index& search_index, const upsert_search_index_options& options = {}) const
       -> std::future<manager_error_context>;
 
+    /**
+     * Drops an index
+     *
+     * @param index_name the name of the search index
+     * @param options optional parameters
+     * @param handler  handler that implements @ref drop_search_index_handler
+     *
+     * @since 1.0.0
+     * @committed
+     */
     void drop_index(std::string index_name, const drop_search_index_options& options, drop_search_index_handler&& handler) const;
 
     [[nodiscard]] auto drop_index(std::string index_name, const drop_search_index_options& options = {}) const
       -> std::future<manager_error_context>;
 
+    /**
+     * Retrieves the number of documents that have been indexed for an index
+     *
+     * @param index_name the name of the search index
+     * @param options optional parameters
+     * @param handler  handler that implements @ref get_indexed_search_index_handler
+     *
+     * @since 1.0.0
+     * @committed
+     */
     void get_indexed_documents_count(std::string index_name,
                                      const get_indexed_search_index_options& options,
                                      get_indexed_search_index_handler&& handler) const;
@@ -79,6 +128,16 @@ class search_index_manager
     [[nodiscard]] auto get_indexed_documents_count(std::string index_name, const get_indexed_search_index_options& options = {}) const
       -> std::future<std::pair<manager_error_context, std::uint64_t>>;
 
+    /**
+     * Pauses updates and maintenance for an index.
+     *
+     * @param index_name the name of the search index
+     * @param options optional parameters
+     * @param handler  handler that implements @ref pause_ingest_search_index_handler
+     *
+     * @since 1.0.0
+     * @committed
+     */
     void pause_ingest(std::string index_name,
                       const pause_ingest_search_index_options& options,
                       pause_ingest_search_index_handler&& handler) const;
@@ -86,6 +145,16 @@ class search_index_manager
     [[nodiscard]] auto pause_ingest(std::string index_name, const pause_ingest_search_index_options& options = {}) const
       -> std::future<manager_error_context>;
 
+    /**
+     * Resumes updates and maintenance for an index.
+     *
+     * @param index_name the name of the search index
+     * @param options optional parameters
+     * @param handler  handler that implements @ref resume_ingest_search_index_handler
+     *
+     * @since 1.0.0
+     * @committed
+     */
     void resume_ingest(std::string index_name,
                        const resume_ingest_search_index_options& options,
                        resume_ingest_search_index_handler&& handler) const;
@@ -93,6 +162,16 @@ class search_index_manager
     [[nodiscard]] auto resume_ingest(std::string index_name, const resume_ingest_search_index_options& options = {}) const
       -> std::future<manager_error_context>;
 
+    /**
+     * Allows querying against an index
+     *
+     * @param index_name the name of the search index
+     * @param options optional parameters
+     * @param handler  handler that implements @ref allow_querying_search_index_handler
+     *
+     * @since 1.0.0
+     * @committed
+     */
     void allow_querying(std::string index_name,
                         const allow_querying_search_index_options& options,
                         allow_querying_search_index_handler&& handler) const;
@@ -100,6 +179,16 @@ class search_index_manager
     [[nodiscard]] auto allow_querying(std::string index_name, const allow_querying_search_index_options& options = {}) const
       -> std::future<manager_error_context>;
 
+    /**
+     * Disallows querying against an index
+     *
+     * @param index_name the name of the search index
+     * @param options optional parameters
+     * @param handler  handler that implements @ref disallow_querying_search_index_handler
+     *
+     * @since 1.0.0
+     * @committed
+     */
     void disallow_querying(std::string index_name,
                            const disallow_querying_search_index_options& options,
                            disallow_querying_search_index_handler&& handler) const;
@@ -107,6 +196,16 @@ class search_index_manager
     [[nodiscard]] auto disallow_querying(std::string index_name, const disallow_querying_search_index_options& options = {}) const
       -> std::future<manager_error_context>;
 
+    /**
+     * Freeze the assignment of index partitions to nodes.
+     *
+     * @param index_name the name of the search index
+     * @param options optional parameters
+     * @param handler  handler that implements @ref  freeze_plan_search_index_handler
+     *
+     * @since 1.0.0
+     * @committed
+     */
     void freeze_plan(std::string index_name,
                      const freeze_plan_search_index_options& options,
                      freeze_plan_search_index_handler&& handler) const;
@@ -114,6 +213,16 @@ class search_index_manager
     [[nodiscard]] auto freeze_plan(std::string index_name, const freeze_plan_search_index_options& options = {}) const
       -> std::future<manager_error_context>;
 
+    /**
+     * Unfreeze the assignment of index partitions to nodes.
+     *
+     * @param index_name the name of the search index
+     * @param options optional parameters
+     * @param handler  handler that implements @ref  unfreeze_plan_search_index_handler
+     *
+     * @since 1.0.0
+     * @committed
+     */
     void unfreeze_plan(std::string index_name,
                        const unfreeze_plan_search_index_options& options,
                        unfreeze_plan_search_index_handler&& handler) const;
@@ -121,6 +230,17 @@ class search_index_manager
     [[nodiscard]] auto unfreeze_plan(std::string index_name, const unfreeze_plan_search_index_options& options = {}) const
       -> std::future<manager_error_context>;
 
+    /**
+     * Allows to see how a document is analyzed against a specific index.
+     *
+     * @param index_name the name of the search index
+     * @param document the document to be analyzed
+     * @param options optional parameters
+     * @param handler  handler that implements @ref analyze_document_handler
+     *
+     * @since 1.0.0
+     * @committed
+     */
     template<typename Document>
     void analyze_document(std::string index_name,
                           Document document,
